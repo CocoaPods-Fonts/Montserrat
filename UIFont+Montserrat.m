593fca0e57828825684d8856f7a23323df58f0e9
Vodovozov Gleb <vodovozovge@gmail.com>
2015-06-10 11:29:38 +0800

    fixed NSBundle URL obtaining. Now it uses [NSBundle bundleForClass:] to get appropriate bundle. It solves the problem when using with cocoapods

--- UIFont+Montserrat.m
+++ UIFont+Montserrat.m
@@ -7,47 +7,56 @@
 #import <CoreText/CoreText.h>
 #import "UIFont+Montserrat.h"
 
+@interface KOSFontLoader : NSObject
 
-@implementation UIFont (OpenSans)
++ (void)loadFontWithName:(NSString *)fontName;
 
-void MontserratLoadFontWithName(NSString *fontName) {
-    NSURL *bundleURL = [[NSBundle mainBundle] URLForResource:@"Montserrat" withExtension:@"bundle"];
+@end
+
+@implementation KOSFontLoader
+
++(void) loadFontWithName:(NSString *)fontName {
+    NSURL *bundleURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Montserrat" withExtension:@"bundle"];
     NSBundle *bundle = [NSBundle bundleWithURL:bundleURL];
     NSURL *fontURL = [bundle URLForResource:fontName withExtension:@"ttf"];
     NSData *fontData = [NSData dataWithContentsOfURL:fontURL];
-
+    
     CGDataProviderRef provider = CGDataProviderCreateWithCFData((CFDataRef)fontData);
     CGFontRef font = CGFontCreateWithDataProvider(provider);
-
+    
     if (font) {
         CFErrorRef error = NULL;
         if (CTFontManagerRegisterGraphicsFont(font, &error) == NO) {
             CFStringRef errorDescription = CFErrorCopyDescription(error);
             @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:(__bridge NSString *)errorDescription userInfo:@{ NSUnderlyingErrorKey: (__bridge NSError *)error }];
         }
-
+        
         CFRelease(font);
     }
-
+    
     CFRelease(provider);
 }
 
-+ (instancetype)montserratLoadAndReturnFont:(NSString *)fontName size:(CGFloat)fontSize onceToken:(dispatch_once_t *)onceToken fontFileName:(NSString *)fontFileName {
-    dispatch_once(onceToken, ^{
-        MontserratLoadFontWithName(fontFileName);
-    });
+@end
 
+@implementation UIFont (Montserrat)
+
++ (instancetype)loadAndReturnFont:(NSString *)fontName size:(CGFloat)fontSize onceToken:(dispatch_once_t *)onceToken fontFileName:(NSString *)fontFileName {
+    dispatch_once(onceToken, ^{
+        [KOSFontLoader loadFontWithName:fontFileName];
+    });
+    
     return [self fontWithName:fontName size:fontSize];
 }
 
 + (instancetype)montserratFontOfSize:(CGFloat)fontSize {
     static dispatch_once_t onceToken;
-    return [self montserratLoadAndReturnFont:@"Montserrat" size:fontSize onceToken:&onceToken fontFileName:@"Montserrat-Regular"];
+    return [self loadAndReturnFont:@"Montserrat" size:fontSize onceToken:&onceToken fontFileName:@"Montserrat-Regular"];
 }
 
 + (instancetype)montserratBoldFontOfSize:(CGFloat)fontSize {
     static dispatch_once_t onceToken;
-    return [self montserratLoadAndReturnFont:@"Montserrat-Bold" size:fontSize onceToken:&onceToken fontFileName:@"Montserrat-Bold"];
+    return [self loadAndReturnFont:@"Montserrat-Bold" size:fontSize onceToken:&onceToken fontFileName:@"Montserrat-Bold"];
 }
 
 @end
